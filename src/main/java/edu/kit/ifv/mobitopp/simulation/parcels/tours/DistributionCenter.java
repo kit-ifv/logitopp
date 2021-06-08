980863577284e1f005f215aa8e5226550eb1e539
Kuebler Jelle IfV <jelle.kuebler@kit.edu>
2021-06-08 19:45:20 +0200

    Fixed bug: use canBeDeliveredTogether to group parcels into deliveries

--- src/main/java/edu/kit/ifv/mobitopp/simulation/parcels/tours/DistributionCenter.java
+++ src/main/java/edu/kit/ifv/mobitopp/simulation/parcels/tours/DistributionCenter.java
@@ -134,7 +134,7 @@ public class DistributionCenter {
 
 		List<IParcel> available = getAvailableParcels(currentTime);
 
-		CollectionsUtil.groupBy(available, IParcel::couldBeDeliveredWith)
+		CollectionsUtil.groupBy(available, IParcel::canBeDeliveredTogether)
 				.forEach(pcls -> deliveries.add(new DeliveryActivityBuilder().addParcels(pcls)));
 
 		return deliveries;
