8bb7fd34f2ce0a0a3c3bc050e02274e40287496b
Jelle Kübler <jelle.kuebler@kit.edu>
2022-12-02 09:39:15 +0100

    Assign each DistributionCenter a VehicleType
    VehicleType specifies the mode of transportation as well as the vehicle
    capacity
    
    currently BIKE, TRUCK, TRAM and OTHER are supported as vehicle type

--- src/main/java/edu/kit/ifv/mobitopp/simulation/fleet/Fleet.java
+++ src/main/java/edu/kit/ifv/mobitopp/simulation/fleet/Fleet.java
@@ -1,5 +1,20 @@
 package edu.kit.ifv.mobitopp.simulation.fleet;
 
+import java.util.Collection;
+
+import lombok.Getter;
+
+@Getter
 public class Fleet {
+	
+	private final Collection<DeliveryVehicle> vehicles;
+	
+	public Fleet() {
+		this.vehicles = null;
+	}
+	
+	public Fleet(Collection<DeliveryVehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
 
 }
