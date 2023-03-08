cc05c6bcb2a5c7774721373458e446d43da000df
Kuebler Jelle IfV <jelle.kuebler@kit.edu>
2023-03-08 14:34:11 +0100

    Fixed TimeWindowDispatchStrategy to return dispatchable tour

--- src/main/java/edu/kit/ifv/mobitopp/simulation/distribution/dispatch/TimeWindowDispatchStrategy.java
+++ src/main/java/edu/kit/ifv/mobitopp/simulation/distribution/dispatch/TimeWindowDispatchStrategy.java
@@ -3,28 +3,43 @@ package edu.kit.ifv.mobitopp.simulation.distribution.dispatch;
 import static edu.kit.ifv.mobitopp.time.DayOfWeek.SUNDAY;
 
 import java.util.Collection;
+import java.util.Optional;
 
-import edu.kit.ifv.mobitopp.simulation.distribution.DistributionCenter;
-import edu.kit.ifv.mobitopp.simulation.distribution.fleet.DeliveryVehicle;
+import edu.kit.ifv.mobitopp.simulation.distribution.fleet.Fleet;
 import edu.kit.ifv.mobitopp.simulation.distribution.tours.PlannedDeliveryTour;
 import edu.kit.ifv.mobitopp.time.Time;
 
 public class TimeWindowDispatchStrategy implements DispatchStrategy {
 	
 	@Override
-	public boolean canDispatch(DistributionCenter center, Time time) {
-		if (time.weekDay().equals(SUNDAY)) {return false;}
+	public Optional<PlannedDeliveryTour> canDispatch(Collection<PlannedDeliveryTour> tours, Fleet fleet, Time time) {
 		
+		if ( isInDispatchHours(time) || isSunday(time) || isFleetAbsent(fleet) || noTourPlanned(tours) ) {
+			return Optional.empty();
+		}
+
+		
+		return tours.stream().filter(t -> endsBeforeEndOfDeliveryTime(time, t)).findFirst();
+	}
+	
+
+	private boolean isInDispatchHours(Time time) {
 		int hour=time.getHour();
-		Collection<PlannedDeliveryTour> plannedTours = center.getStorage().getPlannedTours();
-		Collection<DeliveryVehicle> vehicles = center.getFleet().getVehicles();
-		
-		return !plannedTours.isEmpty() 
-			&& !vehicles.isEmpty() 
-			&& 8 <= hour && hour <= 18 
-			&& plannedTours.stream().anyMatch(t -> endsBeforeEndOfDeliveryTime(time, t));
+		return 8 <= hour && hour <= 18;
 	}
 
+	private boolean isSunday(Time time) {
+		return time.weekDay().equals(SUNDAY);
+	}
+
+	private boolean isFleetAbsent(Fleet fleet) {
+		return fleet.getAvailableVehicles().isEmpty();
+	}
+	
+	private boolean noTourPlanned(Collection<PlannedDeliveryTour> tours) {
+		return tours.isEmpty();
+	}
+	
 	private boolean endsBeforeEndOfDeliveryTime(Time time, PlannedDeliveryTour t) {
 		return time.plus(t.getPlannedDuration()).getHour() <= 21;
 	}
