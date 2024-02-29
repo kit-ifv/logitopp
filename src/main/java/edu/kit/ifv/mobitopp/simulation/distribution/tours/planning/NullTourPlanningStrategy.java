5d714d0548414f681e1bf63513cb3a90e7cbb3be
Jelle Kübler <jelle.kuebler@kit.edu>
2024-02-29 14:33:12 +0100

    Fixed bugs for coordinated tour planning

---
+++ src/main/java/edu/kit/ifv/mobitopp/simulation/distribution/tours/planning/NullTourPlanningStrategy.java
@@ -0,0 +1,25 @@
+package edu.kit.ifv.mobitopp.simulation.distribution.tours.planning;
+
+import java.util.Collection;
+import java.util.List;
+
+import edu.kit.ifv.mobitopp.simulation.distribution.DistributionCenter;
+import edu.kit.ifv.mobitopp.simulation.distribution.fleet.Fleet;
+import edu.kit.ifv.mobitopp.simulation.distribution.tours.PlannedTour;
+import edu.kit.ifv.mobitopp.simulation.parcels.IParcel;
+import edu.kit.ifv.mobitopp.time.Time;
+
+public class NullTourPlanningStrategy implements TourPlanningStrategy {
+
+	@Override
+	public List<PlannedTour> planTours(Collection<IParcel> deliveries, Collection<IParcel> pickUps, Fleet fleet,
+			Time time) {
+		throw new UnsupportedOperationException("NullTourPlanningStrategy.planToursfo " + fleet.getDistributionCenter() + " should never be called!");
+	}
+
+	@Override
+	public boolean shouldReplanTours(DistributionCenter center, Time time) {
+		return false;
+	}
+
+}
