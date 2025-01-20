503d66ec48cae829e88f33e1dfe4dd5c8869796d
ar0305 <ar0305@kit.edu>
2025-01-20 12:25:12 +0100

    changed logit utility functions + parameters of chain preference model based on ahp method results

--- src/main/java/edu/kit/ifv/mobitopp/simulation/distribution/tours/chains/preference/LogitChainPreferenceModel.java
+++ src/main/java/edu/kit/ifv/mobitopp/simulation/distribution/tours/chains/preference/LogitChainPreferenceModel.java
@@ -1,12 +1,13 @@
 package edu.kit.ifv.mobitopp.simulation.distribution.tours.chains.preference;
 
-import static edu.kit.ifv.mobitopp.simulation.distribution.fleet.VehicleType.BIKE;
+import static edu.kit.ifv.mobitopp.simulation.distribution.fleet.VehicleType.*;
 import static edu.kit.ifv.mobitopp.simulation.parcels.ShipmentSize.EXTRA_LARGE;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import edu.kit.ifv.mobitopp.simulation.DeliveryResults;
 import edu.kit.ifv.mobitopp.simulation.ImpedanceIfc;
@@ -14,6 +15,7 @@ import edu.kit.ifv.mobitopp.simulation.distribution.DistributionCenter;
 import edu.kit.ifv.mobitopp.simulation.distribution.chains.TimedTransportChain;
 import edu.kit.ifv.mobitopp.simulation.distribution.chains.TransportChain;
 import edu.kit.ifv.mobitopp.simulation.distribution.tours.chains.CostFunction;
+import edu.kit.ifv.mobitopp.simulation.parcels.BusinessParcel;
 import edu.kit.ifv.mobitopp.simulation.parcels.IParcel;
 import edu.kit.ifv.mobitopp.time.Time;
 import edu.kit.ifv.mobitopp.util.logit.DefaultLogitModel;
@@ -31,12 +33,61 @@ public class LogitChainPreferenceModel implements PreferredChainModel {
 	private final DeliveryResults results;
 	
 	private static int choiceCnt = 0;
+
+	private final double cost_norm_min;
+	private final double cost_norm_max;
+	private final double dist_norm_min;
+	private final double dist_norm_max;
+	private final double time_norm_min;
+	private final double time_norm_max;
+
+	private final double b_cost;
+	private final double b_time;
+	private final double b_dist;
+	private final double b_parcel_size;
+	private final double b_transfers_more_than_one;
+	private final double b_environmental_impact;
+	private final double b_business_recipient_tram;
 	
 	public LogitChainPreferenceModel(ImpedanceIfc impedance, CostFunction costFunction, DeliveryResults results, LogitParameters parameters) {
 		this.impedance = impedance;
 		this.costFunction = costFunction;
 		this.parameters = parameters;
 		this.results = results;
+
+		cost_norm_min = parameters.get("cost_norm_min");
+		cost_norm_max = parameters.get("cost_norm_max");
+		dist_norm_min = parameters.get("dist_norm_min");
+		dist_norm_max = parameters.get("dist_norm_max");
+		time_norm_min = parameters.get("time_norm_min");
+		time_norm_max = parameters.get("time_norm_max");
+
+		b_cost = parameters.get("b_cost");
+		b_time = parameters.get("b_time");
+		b_dist = parameters.get("b_dist");
+		b_parcel_size = parameters.get("b_parcel_size");
+		b_transfers_more_than_one = parameters.get("b_transfers_more_than_one");
+		b_environmental_impact = parameters.get("b_environmental_impact");
+		b_business_recipient_tram = parameters.get("b_business_recipient_tram");
+
+		System.out.println("Log model parameters of LogitChainPreferenceModel:");
+		System.out.println(parameters);
+		System.out.println("cost_norm_min:" + cost_norm_min);
+		System.out.println("cost_norm_max:" + cost_norm_max);
+		System.out.println("dist_norm_min:" + dist_norm_min);
+		System.out.println("dist_norm_max:" + dist_norm_max);
+		System.out.println("time_norm_min:" + time_norm_min);
+		System.out.println("time_norm_max:" + time_norm_max);
+		System.out.println("asc_truck:" + parameters.get("asc_truck"));
+		System.out.println("asc_truck_tram_bike:" + parameters.get("asc_truck_tram_bike"));
+		System.out.println("asc_truck_bike:" + parameters.get("asc_truck_bike"));
+		System.out.println("b_cost:" + b_cost);
+		System.out.println("b_time:" + b_time);
+		System.out.println("b_dist:" + b_dist);
+		System.out.println("b_parcel_size:" + b_parcel_size);
+		System.out.println("b_transfers_more_than_one:" + b_transfers_more_than_one);
+		System.out.println("b_environmental_impact:" + b_environmental_impact);
+		System.out.println("b_business_recipient_tram:" + b_business_recipient_tram);
 	}
 
 	@Override
@@ -81,14 +132,49 @@ public class LogitChainPreferenceModel implements PreferredChainModel {
 		double time = (chain.getTotalDuration() + lastMileTime) / capacity;
 		double dist = (chain.getDistance() + lastMileDist) / capacity;
 		
-		String mode = chain.last().getVehicleType().asString().toLowerCase();
+		String modes = getModeCombinationString(chain);
 
-		double utility = parameters.get("asc_last_"+mode)
-						+ parameters.get("b_cost_" + mode) * cost
-						+ parameters.get("b_time_" + mode) * time
-						+ parameters.get("b_dist_" + mode) * dist;
+		double cost_normalized = (cost-cost_norm_min)/(cost_norm_max-cost_norm_min);
+		double time_normalized = (time-time_norm_min)/(time_norm_max-time_norm_min);
+		double dist_normalized = (dist-dist_norm_min)/(dist_norm_max-dist_norm_min);
 
-		return new UtilResults(utility, cost, time, dist, capacity);
+		double uses_tram = chain.uses(TRAM) ? 1.0 : 0.0;
+		double parcel_size_factor = getParcelFactor(parcel);
+		double transfers_more_than_one = (chain.size() > 2) ? 1.0 : 0.0;
+		double environmental_impact_truck = (chain.lastMileVehicle() == TRUCK) ? 1.0 : 0.0;
+		double is_business_agent = (parcel instanceof BusinessParcel) ? 1.0 : 0.0;
+
+
+		double utility =  parameters.get("asc_" + modes)
+						+ b_cost * cost_normalized
+						+ b_time * time_normalized
+						+ b_dist * dist_normalized
+						+ b_parcel_size * uses_tram * parcel_size_factor
+						+ b_transfers_more_than_one * transfers_more_than_one
+						+ b_environmental_impact * environmental_impact_truck
+						+ b_business_recipient_tram * is_business_agent * uses_tram;
+
+		return new UtilResults(utility, cost_normalized, time_normalized, dist_normalized, capacity);
+	}
+
+	private String getModeCombinationString(TimedTransportChain chain) {
+		return chain.getVehicleTypes().stream().map(t -> t.name().toLowerCase()).collect(Collectors.joining("_"));
+	}
+
+	private double getParcelFactor(IParcel parcel) {
+		switch (parcel.getShipmentSize()) {
+            case SMALL:
+                return 0.0;
+            case MEDIUM:
+                return 0.5;
+            case LARGE:
+                return 1.0;
+            case EXTRA_LARGE:
+            case PALLET:
+            case CONTAINER:
+			default:
+                return 999.0;
+        }
 	}
 	
 	public int getVehicleCapacity(DistributionCenter hub) { //TODO fix vehicle capacity or define mean capacity for estimation?
