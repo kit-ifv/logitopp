147cbf5e7df958910a90f16f9c02b4298af2462a
ar0305 <jelle.kuebler@kit.edu>
2026-02-12 14:38:54 +0100

    added more sophisticated repair routine for csv rows where quantity dos not equal sum of bundle sizes
    in case sum exceeds quantity, iteratively drop bundles at the end of the list, until quantity is matched exactly; if dropping the last bundle size would result in a sum below quantity, subtract the remaining excess instead
    
    in case the sum is below the expected quantity, copy bundles sizes from the start of the list until quantity is matched exactly; in case copying a bundle since would exceed expected quantity, only add the remaining missing amount

--- src/main/java/edu/kit/ifv/mobitopp/simulation/demand/bundling/QuantityAndBundlingCsvReader.java
+++ src/main/java/edu/kit/ifv/mobitopp/simulation/demand/bundling/QuantityAndBundlingCsvReader.java
@@ -3,6 +3,7 @@ package edu.kit.ifv.mobitopp.simulation.demand.bundling;
 import edu.kit.ifv.mobitopp.simulation.business.Business;
 import edu.kit.ifv.mobitopp.simulation.demand.quantity.ParcelQuantityModel;
 import edu.kit.ifv.mobitopp.util.dataimport.CsvFile;
+import lombok.val;
 
 import java.io.File;
 import java.util.*;
@@ -43,13 +44,16 @@ public class QuantityAndBundlingCsvReader implements ParcelQuantityModel<Busines
 
 			int sum = bundling.stream().mapToInt(i -> i).sum();
 			if (sum != quantity) {
-				System.out.println("WARN: the bundling given for " + id + " does not match the given quantity (using only bundles of size 1 instead):\n" +
+                val repaired = repairBundling(bundling, quantity);
+
+				System.out.println("WARN: the bundling given for " + id + " does not match the given quantity (using repaired bundles below):\n" +
 						" - Bundling: " + bundling.stream().map(Object::toString).collect(Collectors.joining(", ")) + "\n" +
 						" - actual sum = " + sum + "\n" +
-						" - expected quantity = " + quantity + "\n"
+						" - expected quantity = " + quantity + "\n" +
+                        " - repaired bundles = " + repaired.stream().map(Object::toString).collect(Collectors.joining(", ")) + "\n"
 				);
 
-				bundling = Collections.nCopies(quantity, 1);
+                bundling = repaired;
 			}
 			
 			this.quantityById.put(id, quantity);
@@ -74,4 +78,74 @@ public class QuantityAndBundlingCsvReader implements ParcelQuantityModel<Busines
 
         return this.bundlesById.getOrDefault(agent.getId(), Collections.emptyList());
 	}
+
+    private List<Integer> repairBundling(List<Integer> bundling, int expectedQuantity) {
+        if (expectedQuantity < 0) {
+            throw new IllegalArgumentException("cannot repair bundling for expected quantity < 0: " + expectedQuantity);
+        }
+
+        List<Integer> repaired = new ArrayList<>();
+        int sum = 0;
+        for (Integer b: bundling) {
+            sum += b;
+            repaired.add(b);
+        }
+
+        if (sum == expectedQuantity) {
+            return repaired;
+        }
+
+        if (sum < expectedQuantity) {
+            return fillWithCopies(expectedQuantity, repaired, sum);
+        }
+
+        return dropBundlesAtEnd(expectedQuantity, sum, repaired);
+    }
+
+    private static List<Integer> dropBundlesAtEnd(int expectedQuantity, int sum, List<Integer> repaired) {
+        int excess = sum - expectedQuantity;
+        int i = repaired.size() - 1;
+
+        while (i > 0 && excess > 0) {
+            int value = repaired.get(i);
+
+            if (value > excess) {
+                repaired.set(i, value - excess);
+                excess = 0;
+
+            } else {
+                excess -= value;
+                repaired.remove(i);
+                i--;
+            }
+
+        }
+
+        return repaired;
+    }
+
+    private static List<Integer> fillWithCopies(int expectedQuantity, List<Integer> repaired, int sum) {
+        if (repaired.isEmpty()) {
+            if (expectedQuantity > 0) {
+                repaired.add(0);
+            }
+            return repaired;
+        }
+
+        int copyIndex = 0;
+        int missing = expectedQuantity - sum;
+        while (missing > 0) {
+            int value = repaired.get(copyIndex++);
+
+            if (value <= missing) {
+                repaired.add(value);
+                missing -= value;
+            } else {
+                repaired.add(missing);
+                missing = 0;
+            }
+        }
+
+        return repaired;
+    }
 }
